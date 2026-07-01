5fd9f6c29da5ee50bdfa0447013cb0ac821fd063
SREEDEVIARYA <aryasreedevi4@gmail.com>
2026-07-01 15:36:05 +0530

    Update settings.py

--- StaffExcelManager/settings.py
+++ StaffExcelManager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pmas=jv2oc(s!k8azg3tx1z(*#x+=*%b93e+a(n)r^_#u5q0^b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".onrender.com"]
 
 
 # Application definition
